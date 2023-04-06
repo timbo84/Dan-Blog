4d2241bafccbc816d1d1ae75fcdd636cdcd1f2ea
Yissel L Nunez Legra <yissel@yissels-mbp.lan>
2023-04-06 14:54:24 -0400

    About page & foooter changes

---
+++ src/components/about/About.jsx
@@ -0,0 +1,33 @@
+import "./about.css";
+
+export default function About() {
+    return (
+        <div className="singlePost">
+            <div className="singlePostWrapper">
+                <img
+                    className="singlePostImg"
+                    src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                    alt=""
+                />
+
+                <h1>Welcome, so good to see you in this website</h1>
+
+                <p className="aboutContent">
+                    My hope in presenting all of these books and blogs is to
+                    offer input to you on some level that will ultimately help your life make more sense, not only for you,
+                    but also in God's observation.
+                    <br /><br />
+                    I hope to inspire you to higher faith, higher expectations, and higher vision. We all are maturing "to the
+                    measure of the stature which belongs to the fullness of Christ," and that is a project of grand
+                    proportions.
+                    <br /><br />
+                    Furthermore, we have to believe that the world is coming increasingly under the influence of the
+                    church, because that is the plan, prophecy, and promise of God. We all get to have a part in adding to
+                    that growing influence. I thank you for joining me, and I wish you well in all that you do.
+                    <br /><br />
+                    Expect great things from God, and attempt great things for God. (William Carey, missionary to India)
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -6,129 +6,139 @@ import Login from "../../pages/login/Login";
 import { Button } from "react-bootstrap";
 
 export default function Topbar() {
-  let {getUserById}= useContext(UserContext);
+  let { getUserById } = useContext(UserContext);
   let params = useParams();
   let navigate = useNavigate()
 
   const [user, setUser] = useState({
-    userId:0,
-    username:"",
-    email:"",
-    firstName:"",
-    lastName:"",
-    createdAt:"",
-    updatedAt:"",
+    userId: 0,
+    username: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    createdAt: "",
+    updatedAt: "",
   });
 
   useEffect(() => {
     async function fetch() {
-        await getUserById(2).then((user) =>
-            setUser(user)
-        );
-        console.log(user);
+      await getUserById(2).then((user) =>
+        setUser(user)
+      );
+      console.log(user);
     }
     fetch();
-}, [Login]);
+  }, [Login]);
 
- function logout() {
-  localStorage.clear();
-   navigate('/')
-   console.log("clicked")
-}
+  function logout() {
+    localStorage.clear();
+    navigate('/')
+    console.log("clicked")
+  }
 
-let Auth = localStorage.getItem("username");
+  let Auth = localStorage.getItem("username");
 
-function displayUser() {
-  if (Auth === user.username) {
-    return (
-      <div className="top">
-      <div className="topLeft">
-        <Link to="/" class="navbar-brand align-items-center">
-          <img src={process.env.PUBLIC_URL + '/Images/logo-vectorV3.png'} height="70" alt="Evince" />
-        </Link>
+  function displayUser() {
+    if (Auth === user.username) {
+      return (
+        <div className="top">
+          <div className="topLeft">
+            <Link to="/" class="navbar-brand align-items-center">
+              <img src={process.env.PUBLIC_URL + '/Images/logo-vectorV3.png'} height="70" alt="Evince" />
+            </Link>
 
-        {/* <i className="topIcon fab fa-facebook-square"></i>
+            {/* <i className="topIcon fab fa-facebook-square"></i>
         <i className="topIcon fab fa-instagram-square"></i>
         <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-twitter-square"></i> */}
-      </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">
-            <Link className="link" to="/">
-              HOME
+          </div>
+          <div className="topCenter">
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/">
+                  HOME
+                </Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/about">
+                  ABOUT
+                </Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/bookstore">
+                  BOOK STORE
+                </Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/write">
+                  WRITE A BLOG
+                </Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" variant="link" onClick={logout}>
+                  LOG OUT
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="topRight">
+            <Link className="link" to="/settings">
             </Link>
-          </li>
-          <li className="topListItem">
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/bookstore">
-              BOOK STORE
+            <ul className="topList">
+            </ul>
+            <i className="topSearchIcon fas fa-search"></i>
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="top">
+          <div className="topLeft">
+            <Link to="/" class="navbar-brand align-items-center">
+              <img src={process.env.PUBLIC_URL + '/Images/logo-vectorV3.png'} height="70" alt="Evince" />
             </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/write">
-              WRITE A BLOG
-            </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" variant="link" onClick={logout}>
-              LOG OUT
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="topRight">
-          <Link className="link" to="/settings">
-          </Link>
-          <ul className="topList">
-          </ul>
-        <i className="topSearchIcon fas fa-search"></i>
-      </div>
-    </div>
-    )
-  }
-  else {
-    return (
-      <div className="top">
-      <div className="topLeft">
-        <Link to="/" class="navbar-brand align-items-center">
-          <img src={process.env.PUBLIC_URL + '/Images/logo-vectorV3.png'} height="70" alt="Evince" />
-        </Link>
 
-        {/* <i className="topIcon fab fa-facebook-square"></i>
+            {/* <i className="topIcon fab fa-facebook-square"></i>
         <i className="topIcon fab fa-instagram-square"></i>
         <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-twitter-square"></i> */}
-      </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">
-            <Link className="link" to="/">
-              HOME
-            </Link>
-          </li>
-          <li className="topListItem">
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/bookstore">
-              BOOK STORE
-            </Link>
-          </li>
-          <li className="topListItem">
-              <Link className="link" to="/A9B8C7D6E5F4G3H2I1">
-                LOGIN
-              </Link>
-            </li>
-        </ul>
-      </div>
-      <div className="topRight">
-        <i className="topSearchIcon fas fa-search"></i>
-      </div>
-    </div>
-    )
+          </div>
+          <div className="topCenter">
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/">
+                  HOME
+                </Link>
+              </li>
+
+              <li className="topListItem">
+                <Link className="link" to="/about">
+                  ABOUT
+                </Link>
+              </li>
+
+              <li className="topListItem">
+                <Link className="link" to="/bookstore">
+                  BOOK STORE
+                </Link>
+              </li>
+
+              <li className="topListItem">
+                <Link className="link" to="/A9B8C7D6E5F4G3H2I1">
+                  LOGIN
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="topRight">
+            <i className="topSearchIcon fas fa-search"></i>
+          </div>
+        </div>
+      )
+    }
   }
-}
 
 
   return (
@@ -136,5 +146,5 @@ function displayUser() {
       {displayUser()}
     </>
   )
-    
+
 }
